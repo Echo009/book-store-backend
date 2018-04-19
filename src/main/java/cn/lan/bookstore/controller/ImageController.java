37bfc9a565e5e6c9bfc4d59600bee65f1904615f
Echo009 <ech0.extreme@foxmail.com>
2018-04-19 15:31:22 +0800

    image upload module

---
+++ src/main/java/cn/lan/bookstore/controller/ImageController.java
@@ -0,0 +1,11 @@
+package cn.lan.bookstore.controller;
+
+/**
+ * Author : Ech0
+ *
+ * @author: Ech0
+ * Email  : ech0.extreme@foxmail.com
+ * Time   : 04/19/2018 03:07 PM
+ */
+public class ImgController {
+}
---
+++ src/main/java/cn/lan/bookstore/util/QiniuUtil.java
@@ -0,0 +1,11 @@
+package cn.lan.bookstore.util;
+
+/**
+ * Author : Ech0
+ *
+ * @author: Ech0
+ * Email  : ech0.extreme@foxmail.com
+ * Time   : 04/19/2018 02:56 PM
+ */
+public class QiniuUtil {
+}
